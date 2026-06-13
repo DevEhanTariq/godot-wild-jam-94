adc49498aa1f65c7898a09d18bc29998f2475922
DevET <ehan.tariq.21@icloud.com>
2026-06-13 16:39:40 +0100

    Feat: Added an info graphic when hovering over a Test Tube (unfinished)
    - Added an info graphic when hovering over a Test Tube (unfinished)

--- ExtraScenes/test_tube_outline.gd
+++ ExtraScenes/test_tube_outline.gd
@@ -12,4 +12,11 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	var Outline = $Button
+	var TestTube = $TestTube
+	var TestTubeLiquid = $TestTube/TestTubeBottomLayer/Liquid
+	
+	var mat = TestTubeLiquid.material as ShaderMaterial
+	mat.set_shader_parameter("tint_color", Color(randf_range(0.0, 1.0), randf_range(0.0, 1.0), randf_range(0.0, 1.0), 1.0))
+	
 	Outline.visible = false
+	TestTube.visible = true
