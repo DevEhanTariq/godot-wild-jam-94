6278b246d4067a9db3a8f96e9ee80629e8bd8b26
DevET <ehan.tariq.21@icloud.com>
2026-06-14 00:25:40 +0100

    Feat: Added Functionality to the Mixer
    - Added Functionality to the Mixer

--- ExtraScenes/mixer.gd
+++ ExtraScenes/mixer.gd
@@ -10,3 +10,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	var ChemicalA = $TestTubeOutline/TestTube
+	var ChemicalB = $TestTubeOutline2/TestTube
+	var NewX = (ChemicalA.CX + ChemicalB.CX)/2
+	var NewY = (ChemicalA.CY + ChemicalB.CY)/2
+	var NewZ = (ChemicalA.CZ + ChemicalB.CZ)/2
+	ChemicalA.CX = NewX
+	ChemicalA.CY = NewY
+	ChemicalA.CZ = NewZ
+	ChemicalB.CX = NewX
+	ChemicalB.CY = NewY
+	ChemicalB.CZ = NewZ
