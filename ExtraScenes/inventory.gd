f6dcfd4250ef3f04a9d199ee571565626a026fe5
DevET <ehan.tariq.21@icloud.com>
2026-06-13 20:45:33 +0100

    Fix: Fixed "Tubes" dictionary
    - Fixed "Tubes" dictionary in the "inventory" scene

--- ExtraScenes/inventory.gd
+++ ExtraScenes/inventory.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-var Tubes = {0:"TestTube",
-1:"TestTube2",
-2:"TestTube3",
-3:"TestTube4",
-4:"TestTube5",
-5:"TestTube6",
-6:"TestTube7"
+var Tubes = {
+0:$TestTube,
+1:$TestTube2,
+2:$TestTube3,
+3:$TestTube4,
+4:$TestTube5,
+5:$TestTube6,
+6:$TestTube7
 }
 
 # Called when the node enters the scene tree for the first time.
@@ -16,5 +17,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	for i in len(Global.TestTubes):
-		pass
+	pass
+	#for i in len(Global.TestTubes):
+	#	var currentTube = Tubes[i]
+	#	currentTube.visible = true
