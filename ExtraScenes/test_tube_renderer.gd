6891589ad5d17422c3b6333a8f5d7ad2f14a15b0
DevET <ehan.tariq.21@icloud.com>
2026-06-14 14:18:15 +0100

    Feat: You can now pick up chemicals and put them in your inventory
    - You can now pick up chemicals and put them in your inventory

--- ExtraScenes/test_tube_renderer.gd
+++ ExtraScenes/test_tube_renderer.gd
@@ -15,9 +15,28 @@ func _process(delta: float) -> void:
 	var Info = $InfoHover
 	var liquid = $TestTubeBottomLayer/Liquid
 	
-	Info.ChemX = CX * Opac
-	Info.ChemY = CY * Opac
-	Info.ChemZ = CZ * Opac
+	Info.ChemX = CX
+	Info.ChemY = CY
+	Info.ChemZ = CZ
 	
 	var mat = liquid.material as ShaderMaterial
 	mat.set_shader_parameter("tint_color", Color(CX * Opac, CY * Opac, CZ * Opac, Opac))
+
+
+func _on_area_2d_mouse_entered() -> void:
+	var Outline = $TestTubeBottomLayer/Liquid/TestTubeTopLayer/Outline
+	Outline.visible = true
+
+func _on_area_2d_mouse_exited() -> void:
+	var Outline = $TestTubeBottomLayer/Liquid/TestTubeTopLayer/Outline
+	Outline.visible = false
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var CurrentInv = get_parent().get_parent()
+			CurrentInv.TestTubes.erase([CX,CY,CZ])
+			Global.TestTubes.append([CX, CY, CZ])
+			get_parent().visible = true
+			get_parent().get_child(0).visible = true
+			visible = false
