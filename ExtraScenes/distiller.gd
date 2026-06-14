914af7f3720b894c7050734622633bb3db1b24a4
DevET <ehan.tariq.21@icloud.com>
2026-06-14 17:29:26 +0100

    Feat: Finished the Distillers Functionality
    - Finished the Distillers Functionality

--- ExtraScenes/distiller.gd
+++ ExtraScenes/distiller.gd
@@ -13,10 +13,15 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	var InTube = $TestTubeOutline/TestTube
+	var Tube = $TestTubeOutline
 	
 	var TubeX = $TestTubeOutline2
 	var TubeY = $TestTubeOutline3
 	var TubeZ = $TestTubeOutline4
+	
+	var InTubeX = $TestTubeOutline2/TestTube
+	var InTubeY = $TestTubeOutline3/TestTube
+	var InTubeZ = $TestTubeOutline4/TestTube
 
 	if InTube.visible and (not $TestTubeOutline2/TestTube.visible) and (not $TestTubeOutline3/TestTube.visible) and (not $TestTubeOutline4/TestTube.visible):
 	
@@ -55,4 +60,42 @@ func _on_button_pressed() -> void:
 		TestTubes.append([0.0, 0.0, 0.0])
 
 	if (not InTube.visible) and ($TestTubeOutline2/TestTube.visible) and ($TestTubeOutline3/TestTube.visible) and ($TestTubeOutline4/TestTube.visible):
-		pass
+		var NewChemX = InTubeX.CX + InTubeY.CX + InTubeZ.CX
+		var NewChemY = InTubeX.CY + InTubeY.CY + InTubeZ.CY
+		var NewChemZ = InTubeX.CZ + InTubeY.CZ + InTubeZ.CZ
+		
+		var NewChem = [NewChemX, NewChemY, NewChemZ]
+		var NewNewChem = []
+		
+		for Chem in NewChem:
+			if Chem > 1.0:
+				NewNewChem.append(1.0)
+			else:
+				NewNewChem.append(Chem)
+				
+		Tube.CX = NewNewChem[0]
+		Tube.CY = NewNewChem[1]
+		Tube.CZ = NewNewChem[2]
+		
+		Tube.distill = true
+		Tube._on_button_pressed()
+		Tube.distill = false	
+		
+		TestTubes.erase([InTubeX.CX, InTubeX.CY, InTubeX.CZ])
+		InTubeX.CX = 0.0
+		InTubeX.CY = 0.0
+		InTubeX.CZ = 0.0
+		TestTubes.append([0.0, 0.0, 0.0])
+		
+		TestTubes.erase([InTubeY.CX, InTubeY.CY, InTubeY.CZ])
+		InTubeY.CX = 0.0
+		InTubeY.CY = 0.0
+		InTubeY.CZ = 0.0
+		TestTubes.append([0.0, 0.0, 0.0])
+		
+		TestTubes.erase([InTubeZ.CX, InTubeZ.CY, InTubeZ.CZ])
+		InTubeZ.CX = 0.0
+		InTubeZ.CY = 0.0
+		InTubeZ.CZ = 0.0
+		TestTubes.append([0.0, 0.0, 0.0])
+		
