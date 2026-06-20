0e8f0a102b5ef8c8d09e6ab9b45825c428beb129
DevET <ehan.tariq.21@icloud.com>
2026-06-20 20:58:54 +0100

    Feat: Added Main Menu
    - Added Main Menu
    - Not yet linked to levels

--- MainScenes/main_menu.gd
+++ MainScenes/main_menu.gd
@@ -1,23 +1,11 @@
-extends Node
+extends Node2D
 
-var win = false
-var fail = false
-
-var Xlow = 0
-var Xhig = 100
-	
-var Ylow = 0
-var Yhig = 100
-
-var Zlow = 0
-var Zhig = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.TestTubes = []
 	var Tube1 = $TestTubeRack/TestTubeOutline2
 	var Tube2 = $TestTubeRack/TestTubeOutline
-	var Tube3 = $TestTubeRack/TestTubeOutline3
 	
 	######################################
 	
@@ -40,33 +28,8 @@ func _ready() -> void:
 	Tube2.distill = false
 	
 	######################################
-	
-	Tube3.CX = randf_range(0.0, 1.0)
-	Tube3.CY = randf_range(0.0, 1.0)
-	Tube3.CZ = randf_range(0.0, 1.0)
-	
-	Tube3.distill = true
-	Tube3._on_button_pressed()
-	Tube3.distill = false
-	
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if win:
-		$"Next Level".visible = true
-	elif fail:
-		$Restart.visible = true
-
-
-func _on_button_pressed() -> void:
-	get_tree().reload_current_scene()
-
-
-func _on_next_level_pressed() -> void:
-	get_tree().change_scene_to_file("res://ExtraScenes/Fire.tscn")
-
-
-func _on_restart_pressed() -> void:
-	get_tree().reload_current_scene()
+	pass
